ef9db83de55282a59d635446c78f90dc13513cb0
Susana <hornosu@yahoo.es>
2025-07-31 19:09:58 +0200

    Arrow animation, still not perfect

--- src/components/general/ArrowButton.jsx
+++ src/components/general/ArrowButton.jsx
@@ -4,9 +4,34 @@ import { useState, useEffect } from 'react';
 
 function ArrowButton() {
     const [arrowPosition, setArrowPosition] = useState(false);
+    const [clicked, setClicked] = useState(false);
+    const [ccw, setCcw]=useState(true);
+    const [cw, setCw]=useState(false);
+    
+    const handleClick = () => {
+        setClicked(true);        
+    };
+
+    useEffect(() => {
+        function arrowAnim() {
+            if (clicked == true) {
+                if (arrowPosition == false) {
+                    setCcw(true);
+                    setCw(false);
+                    setArrowPosition(true);
+                    setClicked(false);
+                } else {
+                    setArrowPosition(false);
+                    setClicked(false);
+                }
+            }
+        }
+        arrowAnim();
+    }, [clicked]);
+
     return(
         <>
-            <img src={arrow} className="arrow"></img>
+            <img src={arrow} onClick={handleClick} className={`arrow ${!arrowPosition ? "ccw-turning" : "cw-turning"}`}></img>
         </>
     )
 }
